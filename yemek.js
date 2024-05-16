6bb5252cd6a5f615020dbca0317cd0d2b6278973
Yildirayalik <yildirayalikara606@gmail.com>
2024-05-17 01:28:43 +0300

    Javascript dosyası oluşturuldu.

---
+++ yemek.js
@@ -0,0 +1,9 @@
+function aciklamaGoster(element) {
+    var aciklama = element.querySelector(".aciklama");
+    aciklama.style.bottom = "0";
+}
+
+function aciklamaGizle(element) {
+    var aciklama = element.querySelector(".aciklama");
+    aciklama.style.bottom = "-150px";
+}
