a829799de49255151123a4df57fc0f829335893e
Yildirayalik <yildirayalikara606@gmail.com>
2024-05-18 18:31:12 +0300

    Giriş dosyasının php dosyası eklendi

---
+++ giris.php
@@ -0,0 +1,28 @@
+<?php
+// Kullanıcı adı ve şifreyi al
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Girilen kullanıcı adının e-posta olup olmadığını kontrol et
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Eğer e-posta adresi doğru formatta değilse hata mesajı göster
+    echo "Lütfen geçerli bir e-posta adresi girin.";
+} 
+// Kullanıcı adı ve şifreyi kontrol et
+else {
+//Eğer doğruysa ekrana Hoşgeldiniz b231210048 yazısını döndür ve kullanıcı butona tıklayınca onu yeniden Giriş sayfasına at.
+    if ($email === 'b231210048@sakarya.edu.tr' && $password === 'b231210048') {
+        echo "Hoşgeldiniz $password";
+        echo '<form action="giris.html">
+        <input type="submit" value="İletişim Sayfasına Git">
+      </form>';
+    } 
+//Eğer yanlışsa ekrana "Yanlış girdiniz lütfen tekrar deneyiniz!" alerti çıksın ve giriş sayfasına otomatik yönlendirsin.
+else {
+        echo '<script>
+        alert("Yanlış girdiniz lütfen tekrar deneyiniz!");
+        window.location.href = "giris.html"; 
+      </script>';
+    }
+}
+?>
